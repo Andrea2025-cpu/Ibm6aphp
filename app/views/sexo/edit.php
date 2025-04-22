30cd5c0056359213e40df5429b291cd0fe7cf7a7
Andrea2025-cpu <andrea.quinonez.tafur@utelvt.edu.ec>
2025-04-22 16:08:36 -0500

    Lista de sexo edit.php

--- app/views/sexo/edit.php
+++ app/views/sexo/edit.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Sexo</title>
+    <title>Editar lista de Sexo</title>
 </head>
 <body>
 
